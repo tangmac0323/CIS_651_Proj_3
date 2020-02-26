75a5fddaac1f1effe90c1a41a2a06ea025476f93
Mengtao Tang <tangmac0323@gmail.com>
2020-02-25 19:32:16 -0500

    Initial Commit

---
+++ CIS_651_Proj_3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CIS_651_Proj_3
+//
+//  Created by Mengtao Tang on 2/25/20.
+//  Copyright © 2020 Mengtao Tang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
